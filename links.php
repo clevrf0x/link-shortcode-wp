ab53ccb50ab60777349b3b6905f8fb6984e3adcb
clevrf0x <favasm72@gmail.com>
2022-11-16 14:04:47 +0530

    Renamed files

--- links.php
+++ links.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Plugin Name:       Social Links
+ * Plugin Name:       Display Links
  * Description:       This plugin includes link shortcode which can be used as anchor tags.
  * Version:           1.0.0
  * Author:            Favas M
  * Author URI:        https://clevrf0x.github.io
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       socials
+ * Text Domain:       display_links
  */
 
 
@@ -19,10 +19,11 @@ function display_links($atts = [], $content = null) {
     ), $atts,
   );
   
+  
   if (! is_null($content)) {
     $out = '<center><a href="' . $link_atts['href'] . '" style="color: var(--global-color-primary); padding: 0 2vw">' . $content . '</a></center>';
   }
-  return $out;
+  return isset($out) ? $out : '';
 }
 
 add_shortcode('link', 'display_links');
